c7e01cbdc66d30fa9d047fc525da5189a5f1d396
mangalagb <mangalagb@gmail.com>
2014-08-19 21:11:03 -0400

    Added highlight for functions

--- secondApp/public/javascripts/highlightText.js
+++ secondApp/public/javascripts/highlightText.js
@@ -1,10 +1,20 @@
-function highlight(text,i)
-{   var ti = i;
+RegExp.escape = function(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+function highlight(newText,i)
+{  
+	var ti = i;
+	var text = newText;
+	
     inputText = document.getElementById("inputText"+ti.toString());
-   // alert(inputText);
+    
+   
     var innerHTML = inputText.innerHTML
     //alert(innerHTML);
-    var re = new RegExp(text, 'g');
+    var newText = RegExp.escape(text);
+   // alert(newText);
+    var re = new RegExp(RegExp.escape(text), 'g');
+    
     innerHTML = innerHTML.replace(re, "<span class='highlight'>"+text+"</span>");
         //alert(innerHTML);
     inputText.innerHTML=innerHTML;
